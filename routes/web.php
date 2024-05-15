c6f99730eb95c4589a9ddd204cede51d83d61fc6
Muhamad Shidiq Maulana <shidiqmuh0@gmail.com>
2024-05-15 23:05:49 +0700

    Menambahkan file baru

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AnimeController;
 
-// Route untuk halaman utama
-Route::get('/', [AnimeController::class, 'index'])->name('home');
-
-// Route untuk menampilkan form tambah anime
-Route::get('/anime/create', [AnimeController::class, 'create'])->name('anime.create');
-
-// Route untuk menyimpan anime baru
-Route::post('/anime', [AnimeController::class, 'store'])->name('anime.store');
-
-// Route untuk menampilkan detail anime
-Route::get('/anime/{id}', [AnimeController::class, 'show'])->name('anime.show');
-
-// Route untuk menampilkan form edit anime
-Route::get('/anime/{id}/edit', [AnimeController::class, 'edit'])->name('anime.edit');
-
-// Route untuk mengupdate anime
-Route::put('/anime/{id}', [AnimeController::class, 'update'])->name('anime.update');
-
-// Route untuk menghapus anime
-Route::delete('/anime/{id}', [AnimeController::class, 'destroy'])->name('anime.destroy');
+Route::get('/', function () {
+    return view('welcome');
+});
